134f9d9ab56332f8ee027ed1a2dc546da5c94cbb
hixcoder <hixcoder@gmail.com>
2022-05-21 13:35:53 +0100

    start philo project

---
+++ philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 10:46:38 by hboumahd          #+#    #+#             */
+/*   Updated: 2022/05/21 10:51:17 by hboumahd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+
+# define PHILO_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+#endif
