67beb904a32e02bd035c5325f312771e23a3f0a4
hixcoder <hixcoder@gmail.com>
2022-05-29 11:28:27 +0100

    pass arg to a thread

--- philo/philo.c
+++ philo/philo.c
@@ -6,61 +6,46 @@
 /*   By: hboumahd <hboumahd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 10:46:35 by hboumahd          #+#    #+#             */
-/*   Updated: 2022/05/29 10:38:00 by hboumahd         ###   ########.fr       */
+/*   Updated: 2022/05/29 11:27:37 by hboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <time.h>
 
-int mails = 0;
-int lock = 0;
-pthread_mutex_t mutex;
+int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
+void *routine(void *arg)
+{
+    int i;
 
-void *role_dice() {
-    int value;
-    int *res;
-
-    pthread_mutex_lock(&mutex);
-    value = (rand() % 6) + 1;
-    res = malloc(sizeof(int));
-    *res = value;
-    printf("this is the adress of res in the fun: %p\n", res);
-    pthread_mutex_unlock(&mutex);
-    return (res);
+    i = *(int *)arg;
+    printf("value of prime in thread %d is %d \n", i, primes[i]);
+    free(arg);
+    return (NULL);
 }
 
 int main()
 {
-    int j = 4;
+    int j = 2;
     pthread_t th[j];
     int i;
-    int *res;
-    
+    int *a;
 
-    // initialize a mutex
-    pthread_mutex_init(&mutex, NULL);
-    
     i = -1;
     while (++i < j)
     {
-        if (pthread_create(&th[i], NULL, &role_dice, NULL) != 0)
+        a = malloc(sizeof(int));
+        *a = i;
+        if (pthread_create(&th[i], NULL, &routine, a) != 0)
             return (1);
-        printf("thread %d has started\n", i);
     }
 
     i = -1;
-     while (++i < j)
+    while (++i < j)
     {
-        if (pthread_join(th[i],(void **) &res) != 0)
+        if (pthread_join(th[i], NULL) != 0)
             return (2);
-        printf("thread %d has finished whit ==> %d\n", i, *res);
-        printf("this is the adress of res in the main: %p\n", res);
-
-        free(res);
     }
-    pthread_mutex_destroy(&mutex);
-     
     return (0);
 }
